68870b1fc834a305790efa85680f0cc7e690447f
Alexander <sashaneo011@gmail.com>
2020-12-04 00:28:25 +0200

    added config

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,20 +1,28 @@
-def test_basic_ddg_search():
-    # Given the DuckDuckGo home page is displayed
-    
+"""
+These tests cover DuckDuckGo searches.
+"""
 
-    # When the user searches for "panda"
-
-
-    # Then the search result title containts "panda"
+from pages.result import DuckDuckGoResultPage
+from pages.search import DuckDuckGoSearchPage
 
 
-    # And the search result query is "panda"
+def test_basic_duckduckgo_search(browser):
+  search_page = DuckDuckGoSearchPage(browser)
+  result_page = DuckDuckGoResultPage(browser)
+  PHRASE = "панда"
 
+  # Given the DuckDuckGo home page is displayed
+  search_page.load()
 
-    # And the search result link pertain to panda
+  # When the user searches for "panda"
+  search_page.search(PHRASE)
 
-     raise Exception("Incomplete Test")
-
-
-
-    
+  # Then the search result title contains "panda"
+  assert PHRASE in result_page.title()
+  
+  # And the search result query is "panda"
+  assert PHRASE == result_page.search_input_value()
+  
+  # And the search result links pertain to "panda"
+  for title in result_page.result_link_titles():
+    assert PHRASE.lower() in title.lower()
